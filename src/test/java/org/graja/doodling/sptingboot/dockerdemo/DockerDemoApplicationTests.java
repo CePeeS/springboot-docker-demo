a8c4f4efca6d0606566848e065315713c7a4b513
Hubert Graja <28177631+CePeeS@users.noreply.github.com>
2022-02-14 10:57:21 +0000

    first commit

---
+++ src/test/java/org/graja/doodling/sptingboot/dockerdemo/DockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.graja.doodling.sptingboot.dockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
